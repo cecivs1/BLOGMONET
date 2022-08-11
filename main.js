3a782b65932d4a8fd1119c1120892da681c649a3
cecivs1 <89472293+cecivs1@users.noreply.github.com>
2022-08-11 13:43:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function changeImageLeave1(){
+    document.getElementById("image1").src = "https://niood.es/wp-content/uploads/2021/10/water-lilies-claude-monet-niood-900x652.jpeg";
+}
+
+function changeImageOver1(){
+    document.getElementById("image1").src = "https://historia-arte.com/_/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbSI6WyJcL2FydHdvcmtcL2ltYWdlRmlsZVwvY2xhdWRlLW1vbmV0LTAzNy5qcGciLCJyZXNpemUsMzAwLDMwMCJdfQ.COGzeGqaiR4Qv0dpQbmYKd27_6TOqD2wPsZR5fAoJBk.jpg";
+}
+
+function changeImageLeave2(){
+    document.getElementById("image2").src = "https://mymodernmet.com/wp/wp-content/uploads/2017/06/monet-paintings-the-japanese-bridge-1899.jpg";
+}
+
+function changeImageOver2(){
+    document.getElementById("image2").src = "https://historia-arte.com/_/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbSI6WyJcL2FydHdvcmtcL2ltYWdlRmlsZVwvY2xhdWRlX21vbmV0XzAzOC5qcGciLCJyZXNpemVDcm9wLDMwMCwyMDAsQ1JPUF9CQUxBTkNFIl19.7ZEvyfv9eAaJBH6Xhb3u-u5sLQlo8bWG196W66M9A40.jpg";
+}
+
+function changeImageLeave3(){
+    document.getElementById("image3").src = "https://artepedrodacruz.files.wordpress.com/2010/05/campos-en-primavera-1887.jpg";
+}
+
+function changeImageOver3(){
+    document.getElementById("image3").src = "https://historia-arte.com/_/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbSI6WyJcL2FydHdvcmtcL2ltYWdlRmlsZVwvNjJlNGVlZmVmMjZkZi5qcGciLCJyZXNpemUsMzAwLDMwMCJdfQ.gnnBFfBR4K_nmeUszzMBCOOwNKF_BB2knLhxAm3b2Qk.jpg";
+}
